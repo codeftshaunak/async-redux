4b887edb6f29f0b5bec64beda1a56465dd10d2ca
codeftshaunak <sagardeyyash19@gmail.com>
2025-07-23 14:28:52 +0600

    refactor fetchFunction for improved readability and consistency

--- fetchfuntion.js
+++ fetchfuntion.js
@@ -1,19 +1,20 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const fetchFunction = async(dispatch, getState)=>{
-    const response = await fetch(
-        'https://jsonplaceholder.typicode.com/todos?_limit=6'
-    );
-    const todos = await response.json();
+const fetchFunction = async (dispatch, getState) => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos?_limit=6"
+  );
+  console.log(response);
+  const todos = await response.json();
 
-    dispatch({
-        type: "todos/todosLoaded", 
-        payload: todos,
-    });
+  dispatch({
+    type: "todos/todosLoaded",
+    payload: todos,
+  });
 
-    console.log(`number of updated todos ${getState().todos.length}`);
-}
+  console.log(`number of updated todos ${getState().todos.length}`);
+};
 
-module.exports={
-    fetchFunction,
-}
+module.exports = {
+  fetchFunction,
+};
